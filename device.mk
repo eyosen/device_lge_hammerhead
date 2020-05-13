08b24331ba55e79e80d34ee0a51b2a571a2cb0d9
Ali B <abittin@gmail.com>
2020-05-13 13:51:50 +0300

    hammerhead: fix board platform for copybit
    It's 8974 not 897

--- device.mk
+++ device.mk
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    copybit.msm897 \
+    copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
     memtrack.msm8974 \
