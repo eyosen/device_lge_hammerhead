d0cbb8f31ab2d5ae8ac992aa7001c60b10e8eabb
z3DD3r <2339201+z3DD3r@users.noreply.github.com>
2021-01-10 12:24:50 +0300

    hammerhead: Use binderized audio HAL

--- device.mk
+++ device.mk
@@ -186,6 +186,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     audio.primary.msm8974 \
     audio.a2dp.default \
     audio.usb.default \
@@ -195,6 +196,7 @@ PRODUCT_PACKAGES += \
 # Audio effects
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
     libqcomvisualizer \
     libqcompostprocbundle
 
